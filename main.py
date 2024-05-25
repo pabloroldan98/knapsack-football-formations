67d973162ae6bd5dcb4ca2db3e06c9c3f4bb93ee
pabloroldan98 <pablo.roldan.peigneux@gmail.com>
2024-05-26 01:01:34 +0200

    fix: Testing teams

--- main.py
+++ main.py
@@ -244,7 +244,7 @@ needed_purge = [player for player in needed_purge if player.name != "Eberechi Ez
 
 # best_full_teams(needed_purge, possible_formations, 30000, verbose=1)
 # best_full_teams(needed_purge, possible_formations, -1, verbose=1)
-# best_full_teams(needed_purge, possible_formations, 300, verbose=2)
+best_full_teams(needed_purge, possible_formations, 300, verbose=2)
 
 
 
