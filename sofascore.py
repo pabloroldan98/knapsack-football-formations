159a24770df521c4702741dcbdec95c71f64d8a2
pabloroldan98 <pablo.roldan.peigneux@gmail.com>
2025-09-04 18:22:04 +0200

    major: added multiple leagues and competitions support

--- sofascore.py
+++ sofascore.py
@@ -290,7 +290,7 @@ def get_player_statistics_rating(player_url):
     return average_rating
 
 
-def get_player_average_rating(player_url, retry_num=1):
+def get_player_average_rating(player_url, headers="default"):
     """
     Given a SofaScore player URL like:
         https://www.sofascore.com/player/unai-marrero/1094782
@@ -309,22 +309,19 @@ def get_player_average_rating(player_url, retry_num=1):
 
     # 2) Fetch seasons info: /api/v1/player/{player_id}/last-year-summary
     seasons_url = f"https://www.sofascore.com/api/v1/player/{player_id}/last-year-summary"
-    headers = {
-        "User-Agent": (
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-            "AppleWebKit/537.36 (KHTML, like Gecko) "
-            "Chrome/91.0.4472.124 Safari/537.36"
-        )
-    }
+    if headers == "default":
+        headers = {
+            "User-Agent": (
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                "AppleWebKit/537.36 (KHTML, like Gecko) "
+                "Chrome/91.0.4472.124 Safari/537.36"
+            )
+        }
     # resp = requests.get(seasons_url, headers=headers, verify=False)
     resp = tls_requests.get(seasons_url, headers=headers, verify=False)
     if resp.status_code == 403: # If blocked by too many calls
-        if retry_num <= 10:
-            print(f"Status: {resp.status_code} , sleeping for 1 minute to avoid block (retry: {retry_num})")
-            time.sleep(60)
-            return get_player_average_rating(player_url, retry_num + 1)
-        else:
-            raise CustomConnectionException(f"HTTP {resp.status_code} when fetching {seasons_url}")
+        print(f"Status: {resp.status_code} , trying with no headers")
+        return get_player_average_rating(player_url, headers=None)
     if resp.status_code != 200:
         # Raise your custom exception if HTTP status is not 200
         raise CustomConnectionException(f"HTTP {resp.status_code} when fetching {seasons_url}")
