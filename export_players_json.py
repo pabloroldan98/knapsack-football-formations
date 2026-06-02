443ff8fa77550375e31319a8980d14269f52aa90
Pablo Roldan <pablo.roldan.peigneux@gmail.com>
2026-06-03 00:43:34 +0200

    fix: add segunda division

--- export_players_json.py
+++ export_players_json.py
@@ -54,6 +54,7 @@ COMPETITIONS = [
     {"key": "ligueone",  "apps": ["biwenger"]},
     {"key": "segunda",   "apps": ["biwenger"]},
     {"key": "champions", "apps": ["biwenger"]},
+    {"key": "mundial", "apps": ["biwenger"]},
 ]
 
 
